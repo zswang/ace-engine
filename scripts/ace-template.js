e8d6dfb269469b5885dabb95b5c1ff87400aaf05
jihu.wang@gmail.com <jihu.wang@gmail.com@8d55cff9-ab97-fdb1-ab7f-8a6fe0c64416>
2011-07-05 16:17:08 +0000

    优化了性能，减少函数调用 增加了helper相关的测试用例

--- scripts/ace-template.js
+++ scripts/ace-template.js
@@ -3,6 +3,15 @@
 <script type="text/template" id="demo2">
 	嵌套的"#{title}"
 </script>
+
+生成结果：
+function anonymous(_output_, _encode_, helper) {
+    with(this) {
+        _output_.push("\t\t\t\u5D4C\u5957\u7684\"", typeof title == "undefined" ? "" : _encode_(title), "\"");
+    }
+}
+----------------------
+模板样本：
 <script type="text/template" id="demo1">
 	<div>
 		语言(#{helper.language})
@@ -23,38 +32,20 @@
 </script>
 
 生成结果：
-function anonymous(_output_, _format_, helper) {
+function anonymous(_output_, _encode_, helper) {
     with(this) {
-        _output_.push("\t\t\t<div>\n\t\t\t\t\u8BED\u8A00(");
-        _output_.push(_format_("#{0}", [helper.language]));
-        _output_.push(")\n\t\t\t\t<a href=\"#\">\n\t\t\t\t\tprint\n\t\t\t\t</a>\n\t\t\t\t");
-        _output_.push(_format_("!#{0}", [helper.format("demo2", this)]));
-        _output_.push("\n");
+        _output_.push("\t\t\t<div>\n\t\t\t\t\u8BED\u8A00(", _encode_(helper.language), ")\n\t\t\t\t<a href=\"#\">\n\t\t\t\t\tprint\n\t\t\t\t</a>\n\t\t\t\t", helper.format("demo2", this), "\n");
         switch (status) {
         case "edit":
-            _output_.push("\t\t\t\t\t\t<input type=\"text\" value=\"");
-            _output_.push(_format_("#{0}", [typeof title == "undefined" ? "" : title]));
-            _output_.push("\"/><input type=\"button\" value=\"\u786E\u5B9A\"/>\n");
+            _output_.push("\t\t\t\t\t\t<input type=\"text\" value=\"", typeof title == "undefined" ? "" : _encode_(title), "\"/><input type=\"button\" value=\"\u786E\u5B9A\"/>\n");
             break;
         case "normal":
-            _output_.push("\t\t\t\t\t\t<span>");
-            _output_.push(_format_("#{0}", [typeof title == "undefined" ? "" : title]));
-            _output_.push("</span><input type=\"button\" value=\"\u7F16\u8F91\"/>\n");
+            _output_.push("\t\t\t\t\t\t<span>", typeof title == "undefined" ? "" : _encode_(title), "</span><input type=\"button\" value=\"\u7F16\u8F91\"/>\n");
             break;
         default:
             ;
         }
-        _output_.push("\t\t\t\t<span>");
-        _output_.push(_format_("!#{0}", [typeof html == "undefined" ? "" : html]));
-        _output_.push("</span>\n\t\t\t</div>");
-    }
-}
-
-function anonymous(_output_, _format_, helper) {
-    with(this) {
-        _output_.push("\t\t\t\u5D4C\u5957\u7684\"");
-        _output_.push(_format_("#{0}", [typeof title == "undefined" ? "" : title]));
-        _output_.push("\"");
+        _output_.push("\t\t\t\t<span>", typeof html == "undefined" ? "" : html, "</span>\n\t\t\t</div>");
     }
 }
 */
@@ -67,7 +58,7 @@ var AceTemplate = AceTemplate || {};
 	 * 一套基于HTML和JS语法自由穿插的模板系统
 	 * http://code.google.com/p/ace-engine/wiki/AceTemplate
 	 * @author 王集鹄(wangjihu，http://weibo.com/zswang) 鲁亚然(luyaran，http://weibo.com/zinkey)
-	 * @version 2011-07-02 
+	 * @version 2011-07-06 
  	 * @copyright (c) 2011, Baidu Inc, All rights reserved.
 	 */
 
@@ -123,6 +114,16 @@ var AceTemplate = AceTemplate || {};
 			return String(html).replace(/["<>& ]/g, function(all) {
 				return "&" + htmlEncodeDict[all] + ";";
 			});
+		},
+		/**
+		 * 获得元素文本
+		 * @param {Element} element
+		 */
+		elementText: function(element) {
+			if (!element || !element.tagName) return "";
+			if (/^(input|textarea)$/i.test(element.tagName))
+				return element.value;
+			return lib.decodeHTML(element.innerHTML);
 		}
 	};
 	
@@ -136,32 +137,6 @@ var AceTemplate = AceTemplate || {};
 	 */
 	var registerAll = false;
 
-	/**
-	 * 格式化数据
-	 * @param {String} template 模板字符串
-	 * @param {Object} json 数据
-	 */
-	function format(template, json) {
-		return template.replace(/(!?#)\{(.*?)\}/g, function(all, flag, key) {
-			var text = json && (key in json) ? json[key] : ""; // undefined 则取 ""
-			switch (flag) {
-				case "#": return lib.encodeHTML(text);
-				case "!#": return text;
-			}
-		});
-	}
-
-	/**
-	 * 获得元素文本
-	 * @param {Element} element
-	 */
-	function elementText(element) {
-		if (!element || !element.tagName) return "";
-		if (/^(input|textarea)$/i.test(element.tagName))
-			return element.value;
-		return lib.decodeHTML(element.innerHTML);
-	}
-	
 	/**
 	 * 构造模板的处理函数
 	 * 不是JS块的规则
@@ -189,21 +164,25 @@ var AceTemplate = AceTemplate || {};
 			.replace(/[\r\n]+/g, "\n") // 去掉多余的换行，并且去掉IE中困扰人的\r
 			.replace(/^\n+|\s+$/mg, "") // 去掉空行，首部空行，尾部空白
 			.replace(/((^\s*[<>!#^&\u0000-\u0008\u007F-\uffff].*$|^.*[<>]\s*$|^(?!\s*(else|do|try|finally)\s*$)[^'":;,\[\]{}()\n\/]+$|^(\s*(([\w-]+\s*=\s*"[^"]*")|([\w-]+\s*=\s*'[^']*')))+\s*$|^\s*([.#][\w-.]+(:\w+)?(\s*|,))*(?!(else|do|while|try|return)\b)[.#]?[\w-.*]+(:\w+)?\s*\{.*$)\s?)+/mg, function(expression) { // 输出原文
-				return ['_output_.push("',	expression
+				expression = ['"', expression
 					.replace(/&none;/g, "") // 空字符
 					.replace(/["'\\]/g, "\\$&") // 处理转义符
 					.replace(/\n/g, "\\n") // 处理回车转义符
 					.replace(/(!?#)\{(.*?)\}/g, function (all, flag, template) { // 变量替换
-						template = template.replace(/\\n/g, "\n").replace(/\\([\\'"])/g, "$1") // 还原转义
-						if (/^[a-z$][\w+$]+$/i.test(template) && !(/^(true|false|NaN|null|this)$/.test(template))) { // 单纯变量，加一个未定义保护
-							template = format('typeof !#{0}=="undefined"?"":!#{0}', [template]);
-						}
-						return format('");_output_.push(_format_("!#{0}{0}",[!#{1}]));_output_.push("', [flag, template]);
-					}),
-				'");'].join("").replace(/_output_.push\(""\);/g, "");
+						template = template.replace(/\\n/g, "\n").replace(/\\([\\'"])/g, "$1"); // 还原转义
+						var identifier = /^[a-z$][\w+$]+$/i.test(template) &&
+							!(/^(true|false|NaN|null|this)$/.test(template)); // 单纯变量，加一个未定义保护
+						return ['",', 
+							identifier ? ['typeof ', template, '=="undefined"?"":'].join("") : "", 
+							(flag == "#" ? '_encode_' : ""), 
+							'(', template, '),"'].join("");
+					}), '"'].join("").replace(/^"",|,""$/g, "");
+				if (expression)	
+					return ['_output_.push(', expression, ');'].join("");
+				else return "";
 			}));
 		body.push("}");
-		var result = new Function("_output_", "_format_", "helper", body.join(""));
+		var result = new Function("_output_", "_encode_", "helper", body.join(""));
 		/* Debug Start */
 		logger.log(String(result));
 		/* Debug End */
@@ -226,7 +205,7 @@ var AceTemplate = AceTemplate || {};
 		helper = helper || this; // 默认附加数据
 		reader = readerCaches[id]; // 优先读取缓存
 		if (!reader) { // 缓存中未出现
-			if (/^[\w-]+$/.test(id)) { // 合法的标识符按id读取
+			if (!/[^\w-]/.test(id)) { // 合法的标识符按id读取
 				if (!element) {
 					element = lib.g(id);
 				}
@@ -236,7 +215,7 @@ var AceTemplate = AceTemplate || {};
 			} 			
 		}
 		var output = [];
-		reader.call(data || "", output, format, helper);
+		reader.call(data || "", output, lib.encodeHTML, helper);
 		return output.join("");
 	};
 	
@@ -265,7 +244,7 @@ var AceTemplate = AceTemplate || {};
 			if (typeof target == "undefined") {
 				target = lib.g(id);
 			}
-			target = elementText(target);
+			target = lib.elementText(target);
 		}
 		return readerCaches[id] = analyse(target);	
 	};
